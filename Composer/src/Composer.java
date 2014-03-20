eeed80ea53a0fd58232e839f3031176230eeee3e
jzhaoaf <jzhaoaf@lbe020.ust.hk>
2014-03-20 23:08:05 +0800

    Add basic class

--- Composer/src/Composer.java
+++ Composer/src/Composer.java
@@ -1,4 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 public class Composer {
+	
+	static String filename;
+	static String readInPath = " /Users/jzhaoaf/Desktop/";
+	static String outputPath;
+	int MIN = 3;
+	int MAX = 20;
+	static int THRESHOLD = 15;
+	
+	public static void main(String[] args) {
 
+		try {
+			singleTest();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//batchTest(Folder);
+		
+	}
+
+	private static void singleTest() throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		readInPatterns("/Users/jzhaoaf/Desktop/allPattern15.txt");
+	}
+
+	private static void readInPatterns(String filename) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(new File(filename));
+		String line;
+		while(s.hasNextLine()) {
+			line = s.nextLine();
+			//System.out.println(line);
+		}
+		
+	}
+	
 }
---
+++ Composer/src/SequencePair.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+
+public class SequencePair {
+	
+	public ArrayList<Integer> lrcSeq;
+	public ArrayList<Integer> meloSeq;
+	
+	public SequencePair(ArrayList<Integer> lrcSeq, ArrayList<Integer> meloSeq) {
+		this.lrcSeq = lrcSeq;
+		this.meloSeq = meloSeq;
+	}
+}
