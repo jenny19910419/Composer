956cf3a72f5a51984ed1476740387cae0f05d3b9
jzhaoaf <jzhaoaf@lbe022.ust.hk>
2014-03-21 23:04:52 +0800

    generate midi

--- Composer/src/Composer.java
+++ Composer/src/Composer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -52,9 +53,54 @@ public class Composer {
 	private static Sequence generateSequence(String filename) {
 		// TODO Auto-generated method stub
 		//generate sequence for lrc files
+		ArrayList<Integer> lrc = new ArrayList<Integer>();
+		ArrayList<Integer> melo = new ArrayList<Integer>();
+		
+		//suppose given lrc sequence; change later
+		Random random = new Random();
+		for(int i = 0 ; i < 100; ++i) {
+			lrc.add(new Integer(random.nextInt(10)));
+		}
+		
+		
+		
+		
+		
 		
 		
 		return null;
+	}
+	
+	public static String lcs(String a, String b) {
+	    int[][] lengths = new int[a.length()+1][b.length()+1];
+	 
+	    // row 0 and column 0 are initialized to 0 already
+	 
+	    for (int i = 0; i < a.length(); i++)
+	        for (int j = 0; j < b.length(); j++)
+	            if (a.charAt(i) == b.charAt(j))
+	                lengths[i+1][j+1] = lengths[i][j] + 1;
+	            else
+	                lengths[i+1][j+1] =
+	                    Math.max(lengths[i+1][j], lengths[i][j+1]);
+	 
+	    // read the substring out from the matrix
+	    StringBuffer sb = new StringBuffer();
+	    for (int x = a.length(), y = b.length();
+	         x != 0 && y != 0; ) {
+	        if (lengths[x][y] == lengths[x-1][y])
+	            x--;
+	        else if (lengths[x][y] == lengths[x][y-1])
+	            y--;
+	        else {
+	            assert a.charAt(x-1) == b.charAt(y-1);
+	            sb.append(a.charAt(x-1));
+	            x--;
+	            y--;
+	        }
+	    }
+	 
+	    return sb.reverse().toString();
 	}
 
 	private static void singleTest() throws FileNotFoundException {
